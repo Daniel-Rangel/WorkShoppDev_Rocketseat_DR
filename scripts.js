eec298b43568f0e4da978c4684b0ed9ddea05e58
Daniel Rangel <danf.arangel@gmail.com>
2020-04-02 19:35:30 -0300

    Aula 2 modelagem do site

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
-document
-.querySelector("button.fat")
-.addEventListener("click", function(){
-  document.querySelector("#footer").classList.toggle("hide")
-});
+function onOff(){
+  document.querySelector("#modal")
+  .classList
+  .toggle("hide")
+}
